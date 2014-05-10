9202db61cc36cf0fcba763acc58d61bb1e5e7f7a
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2014-05-10 22:52:46 +0300

    new readme

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 just-a-data-collection-engine
 =============================
 
-This is just a data collection engine/web service in Django
+This is just a data collection web service in Django.  
+The application that comes with it collects info about alien sightings.
+
+the `jadce` folder contains the django-project settings,  
+and `area51` the application
+
+
+AUTHORS
+-------
+
+* papanikge@ceid.upatras.gr
+* gryllos@ceid.upatras.gr
--- jadce/settings.py
+++ jadce/settings.py
@@ -12,23 +12,16 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '^usd)$-79=cg5wu2w0i()=sjxsfd#nn9)uxrsfbh6#n0iiu5c3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +48,6 @@ WSGI_APPLICATION = 'jadce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
