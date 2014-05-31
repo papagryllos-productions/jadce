0ee29972b6d03910d5eae041c5b94e304eba4b81
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2014-05-31 20:26:30 +0300

    fix gridding system; add map; minor changes

---
+++ area51/static/js/maps.js
@@ -0,0 +1,20 @@
+function initialize() {
+  /* position Amsterdam */
+  var latlng = new google.maps.LatLng(52.3731, 4.8922);
+
+  var mapOptions = {
+    center: latlng,
+    scrollWheel: false,
+    zoom: 13
+  };
+
+  var marker = new google.maps.Marker({
+    position: latlng,
+    url: '/',
+    animation: google.maps.Animation.DROP
+  });
+
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  marker.setMap(map);
+};
+google.maps.event.addDomListener(window, 'load', initialize);
