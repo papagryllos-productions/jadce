f600a14d8bcd5013eac0050e542d2d180c57ffa6
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2014-06-01 14:36:26 +0300

    fix image urls

--- jadce/settings.py
+++ jadce/settings.py
@@ -68,6 +68,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "area51", "static")
 
 # Media upload location (usually "/var/www/example.com/media/"):
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
