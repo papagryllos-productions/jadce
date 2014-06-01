9c8e7cbd02b84b0e9fdf6552f800c51982254c8f
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2014-06-01 20:06:49 +0300

    fix url issues with slashes

--- area51/urls.py
+++ area51/urls.py
@@ -3,7 +3,7 @@ from area51 import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
-    url(r'^create_account$', views.create_account, name='create_account'),
+    url(r'^create_account/$', views.create_account, name='create_account'),
     url(r'^u/(?P<username>\w+)/$', views.user, name='user'),
-    url(r'^api/data$', views.data, name='data'),
+    url(r'^api/data/$', views.data, name='data'),
 )
