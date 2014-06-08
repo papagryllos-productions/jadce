f1a2b81d752fb5e7fa3cafea8975a7a6c90f1c45
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2014-06-08 15:02:15 +0300

    dynamically add the extra attribute to allow phone camera usage

--- area51/static/js/dynamic.js
+++ area51/static/js/dynamic.js
@@ -1,7 +1,10 @@
+/* Function for dynamically requesting data and displaying them */
 function updateData() {
   $.get('/api/data', function(data) {
     $('#dynamic').html(data);
   });
 }
-
 setInterval("updateData()", 1000);
+
+/* Adding the extra attribute to the image upload buttons. Freaking django. */
+$('#id_photo').attr("accept", "image/*;capture=camera");
