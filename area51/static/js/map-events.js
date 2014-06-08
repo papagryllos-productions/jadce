01eb2230e352fffb0faa8af3bf4d492605d3a9d4
nacho <gryllosprokopis@gmail.com>
2014-06-08 20:07:01 +0300

    Event page maps centers on event marker

---
+++ area51/static/js/map-events.js
@@ -0,0 +1,19 @@
+function initialize() {
+    /* event page map initialization */
+    var position = $("#position").text();
+    var pos = position.split(',');
+    var location = new google.maps.LatLng(pos[0], pos[1]);
+    var mapOptions = {
+        center: location,
+        scrollWheel: true,
+        zoom: 12
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
+    $("#position").hide();
+};
+
+google.maps.event.addDomListener(window, 'load', initialize);
