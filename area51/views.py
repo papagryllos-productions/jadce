3485319db34ef1e0420303583a96c3c272fd1936
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2014-06-13 14:46:45 +0300

    minor fixes on tables, link, indentations, alerts
    -- Also change the sorting in the main page event list
    -- Added colors on lists

--- area51/views.py
+++ area51/views.py
@@ -12,7 +12,7 @@ import area51.models as M
 # The main page
 def home(request):
     latest_events = M.Event.objects.all().order_by('-date_of_creation')[:20]
-    context = {'latest_events': latest_events}
+    context = {'latest_events': reversed(latest_events)}
     return render(request, 'area51/index.html', context)
 
 # user profile view
